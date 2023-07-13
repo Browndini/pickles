bfa63f31e59d336df04fadcea8aa4660e24dcd79
kyle <dtkylebrown@gmail.com>
2023-07-12 23:39:27 -0700

    pickles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='left-section'>
+          <img src={'./BT-Secondary.png'} className="App-logo" alt="logo" />
+        </div>
       </header>
+        <p className='content-pickle'>
+        <img src={'./Pickle.png'} className="pickle" alt="Pickle" />
+        </p>
     </div>
   );
 }
